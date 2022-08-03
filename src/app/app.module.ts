9eb07eb0237f94fd200fb2ae40e59622b81d2a95
AbhinavChandran98 <abhinavchandran0@gmail.com>
2022-08-04 00:29:53 +0530

    nav bar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCourseComponent } from './add-course/add-course.component';
 import { ViewCourseComponent } from './view-course/view-course.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+const appRoutes:Routes=[
+  {
+    path:"",
+    component:ViewCourseComponent
+  },
+  {
+    path:"Add",
+    component:AddCourseComponent
+  }
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     AddCourseComponent,
-    ViewCourseComponent
+    ViewCourseComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
